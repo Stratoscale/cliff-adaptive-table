f12b95889421fef8d6fe956790cbaae894ebadcf
Lior Segev <lior@stratoscale.com>
2018-09-18 09:25:05 +0300

    added split-table modifier
    if true, this modifier allows tables to be split to multiple tables,
    each with some of the columns. for example, if the standard table:
    
    +------+----------------+------+------+
    | col1 | col2           | col3 | col4 |
    +======+================+======+======+
    | val1 | long value two | val3 | val4 |
    +------+----------------+------+------+
    
    is too wide, it is turned into
    
    +------+----------------+
    | col1 | col2           |
    +======+================+
    | val1 | long value two |
    +------+----------------+
      +------+------+
      | col3 | col4 |
      +======+======+
      | val3 | val4 |
      +------+------+

--- cliff_adaptive_table/adaptive_table_def.py
+++ cliff_adaptive_table/adaptive_table_def.py
@@ -1,9 +1,12 @@
 class AdaptiveTableDef(object):
-    def __init__(self, widths, depth, compact, force_frames, horizontal_lines, headers):
+    def __init__(self, widths, depth, compact, force_frames, horizontal_lines, indent, headers):
+        self._widths = widths
         self._depth = depth
+        self._compact = compact
         self._headers = headers
         self._force_frames = force_frames
         self._horizontal_lines = horizontal_lines
+        self._indent = indent
         self._set_separators(widths, depth, compact, force_frames)
 
     def _set_separators(self, widths, depth, compact, force_frames):
@@ -15,8 +18,10 @@ class AdaptiveTableDef(object):
         sep_edges = '%s' if compact else '-%s-'
         if (depth == 0 and not compact) or force_frames:
             sep_edges = '+%s+' % sep_edges
-        self.line_format = line_edges % (line_joiner.join(['%%-%ds' % width for width in widths]))
-        self._line_separator = sep_edges % (sep_joiner.join(['-' * width for width in widths]))
+        line_format = line_edges % (line_joiner.join(['%%-%ds' % width for width in widths]))
+        self.line_format = (' ' * self._indent) + line_format
+        line_separator = sep_edges % (sep_joiner.join(['-' * width for width in widths]))
+        self._line_separator = (' ' * self._indent) + line_separator
         self._header_separator = self._line_separator.replace('-', '=')
 
     def _separator_needed(self, row_index, max_lines, prev_max_lines):
@@ -37,3 +42,7 @@ class AdaptiveTableDef(object):
 
     def get_end_separator(self):
         return self._line_separator
+
+    def total_width(self):
+        per_column = 1 if self._compact else 3
+        return self._indent + sum(self._widths) + per_column * len(self._widths) + per_column - 2
